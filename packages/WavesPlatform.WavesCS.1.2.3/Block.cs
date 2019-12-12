c151a87cc234cfe14aeb7561d6db777d76190356
d-kiselev <dkiselev1234@gmail.com>
2019-12-12 10:34:09 +0300

    change folder structure

---
+++ packages/WavesPlatform.WavesCS.1.2.3/Block.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WavesCS
+{
+    public class Block
+    {
+        char chainId;
+        long count;
+
+        public Block(char chainId, long count=100)
+        {
+            this.chainId = chainId;
+            this.count = count;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new BlocksEnumerator(chainId, count);
+        }
+    }
+
+
+    public class BlocksEnumerator : IEnumerator
+    {
+        Node node;
+        long height = -1;
+        long position = -1;
+        long count = -1;
+
+
+        public BlocksEnumerator(char chainId, long count=100)
+        {
+            node = new Node(chainId);
+            height = node.GetHeight();
+            position = height + 1;
+            this.count = count;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (height == -1 || count == -1)
+                    throw new InvalidOperationException();
+                return node.GetBlockTransactionsAtHeight(position);
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (position <= height + 1 && position > height - count)
+            {
+                position--;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ waves-mining-probability/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WavesCS;
+
+namespace wavesminingprobability
+{
+    static class NodeExtentions
+    {
+        public static decimal GetGenBalance(this Node node, string address)
+        {
+            return node.GetObject($"addresses/effectiveBalance/{address}/1000").GetLong("balance");
+        }
+
+        public static IEnumerable<Dictionary<string, object>> GetHeadersSequence(this Node node, long from, long to)
+        {
+            return node.GetObjects($"blocks/headers/seq/{from}/{to}");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string address;
+
+            if (args.Length == 0)
+            {
+                Console.Write("address = ");
+                address = Console.ReadLine();
+            }
+            else
+            {
+                address = args[0];
+                Console.Write($"address = {address}");
+            }
+
+            const long millisecondsInOneDay = 24 * 60 * 60 * 1000;
+            const long expectedBlocksInOneDay = 24 * 60;
+
+            Node node = new Node(Node.MainNetChainId);
+
+            var currentHeight = node.GetHeight();
+            var currentTimestamp = node.GetBlockTimestamp(currentHeight);
+
+            var firstBlockHeight = currentHeight - expectedBlocksInOneDay;
+            var firstBlockTimestamp = node.GetBlockTimestamp(firstBlockHeight);
+
+            while (currentTimestamp - firstBlockTimestamp < millisecondsInOneDay)
+            {
+                firstBlockHeight--;
+                firstBlockTimestamp = node.GetBlockTimestamp(firstBlockHeight);
+            }
+
+            while (currentTimestamp - firstBlockTimestamp > millisecondsInOneDay)
+            {
+                firstBlockHeight++;
+                firstBlockTimestamp = node.GetBlockTimestamp(firstBlockHeight);
+            }
+
+            var minedBlocks = 0;
+            var generators = new Dictionary<string, bool>();
+
+            const long maxSequenceLength = 100;
+
+            for (var h = firstBlockHeight; h <= currentHeight; h += maxSequenceLength)
+            {
+                var headers = node.GetHeadersSequence(h,Math.Min(h + maxSequenceLength - 1, currentHeight));
+
+                foreach (var generator in headers.Select(header => header.GetString("generator")))
+                {
+                    generators[generator] = true;
+                }
+
+                minedBlocks += headers.Count(header => header.GetString("generator") == address);
+            }
+
+            var totalGenBalance = generators.Keys.Sum(node.GetGenBalance);
+            var share = node.GetGenBalance(address) / totalGenBalance;
+            var numBlocks = currentHeight - firstBlockHeight + 1;
+            var estimatedBlocks = share * numBlocks;
+            var performanceRatio = minedBlocks / estimatedBlocks;
+
+            Console.WriteLine($"number of blocks = {numBlocks}");
+            Console.WriteLine($"stake share = {Math.Round(share * 100, 2)}%");
+            Console.WriteLine($"estimated blocks = {Math.Round(estimatedBlocks, 2)}");
+            Console.WriteLine($"mined blocks = {minedBlocks}");
+            Console.WriteLine($"performance ratio = {Math.Round(performanceRatio, 2)}");
+        }
+    }
+}
